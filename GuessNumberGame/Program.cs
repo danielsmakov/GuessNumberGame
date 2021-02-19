822b05fbb4466937a0822a3a09041856e55cd44b
danielsmakov <54620863+danielsmakov@users.noreply.github.com>
2021-02-19 22:02:26 +0600

    Add the attempt limiter

--- GuessNumberGame/Program.cs
+++ GuessNumberGame/Program.cs
@@ -6,28 +6,43 @@ namespace GuessNumberGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, stranger! This is the \"Guess Number\" game. It involves two players. Press \"Enter\" to start!");
+            // Instructions for players
+            Console.WriteLine("Hello, strangers! This is the \"Guess Number\" game. It involves two players. Press \"Enter\" to start!");
             Console.ReadKey();
+
+            // Setting the limiter of attempts
+            Console.WriteLine("Before you start to play, please, set the maximum number of attempts:");
+            string input = Console.ReadLine();
+            int attemptLimiter = ValidateInput(input);
+            Console.WriteLine("Press \"Enter\" to start the game.");
+            Console.ReadKey();
+            Console.Clear();
 
             //Player 1 part
             Console.WriteLine("Player 1, please, enter the integer number: ");
 
-            string input = Console.ReadLine();
+            input = Console.ReadLine();
 
             int theNumber = ValidateInput(input);
 
             Console.WriteLine("Great, the number is valid! Press \"Enter\" to continue.");
+            Console.ReadKey();
             Console.Clear();
 
             //Player 2 part
-            Console.WriteLine("Player 2, you should guess the number of Player 1.");
+            Console.WriteLine($"Player 2, you should guess the number of Player 1. You have {attemptLimiter} attempts.");
 
             int guess;
-            int guessCounter = 0;
+            int attemptCounter = 0;
             do
             {
-                guessCounter++;
-                Console.WriteLine($"Attempt #{guessCounter}");
+                if (attemptCounter == attemptLimiter)
+                {
+                    Console.WriteLine($"You loose! You spent all {attemptLimiter} attempts.");
+                    break;
+                }
+                attemptCounter++;
+                Console.WriteLine($"Attempt #{attemptCounter}");
                 Console.WriteLine("Enter the integer number: ");
                 input = Console.ReadLine();
                 guess = ValidateInput(input);
@@ -40,9 +55,13 @@ namespace GuessNumberGame
                 {
                     Console.WriteLine("Your guess is LESS than the number. Try again.\n");
                 }
+                else
+                {
+                    Console.WriteLine("Congratulations! You win!");
+                    Console.WriteLine($"You spent {attemptCounter} out of {attemptLimiter} attempts.");
+                }
             } while (guess != theNumber);
-            Console.WriteLine("Congratulations! You win!");
-            Console.WriteLine($"Number of attempts: {guessCounter}");
+            
             Console.ReadKey();
         }
         public static int ValidateInput(string str)
