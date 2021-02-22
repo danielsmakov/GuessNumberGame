456d5b16a779c059e653681d56511fa676f8cbf1
danielsmakov <54620863+danielsmakov@users.noreply.github.com>
2021-02-22 15:27:25 +0600

    Add the players' names entering, add xml-comments to methods, change the ValidateInputIntegerNumber method

--- GuessNumberGame/Program.cs
+++ GuessNumberGame/Program.cs
@@ -6,46 +6,69 @@ namespace GuessNumberGame
     {
         static void Main(string[] args)
         {
+            string firstPlayerName = string.Empty;
+            string secondPlayerName = string.Empty;
+            string input;
+
             // Instructions for players
             Console.WriteLine("Hello, strangers! This is the \"Guess Number\" game. It involves two players. Press \"Enter\" to start!");
             Console.ReadKey();
 
+            // Entering the players' names
+            Console.WriteLine("Player 1, please, enter your name: ");
+            input = Console.ReadLine();
+            firstPlayerName = ValidateInputString(input);
+            Console.WriteLine($"Nice name, {firstPlayerName}! Press \"Enter\" to continue!");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Player 2, please, enter your name: ");
+            input = Console.ReadLine();
+            secondPlayerName = ValidateInputString(input);
+            Console.WriteLine($"Cool, {secondPlayerName}! Press \"Enter\" to continue!");
+            Console.ReadKey();
+            Console.Clear();
+
             // Setting the limiter of attempts
             Console.WriteLine("Before you start to play, please, set the maximum number of attempts:");
-            string input = Console.ReadLine();
-            int attemptLimiter = ValidateInput(input);
+            input = Console.ReadLine();
+            input = ValidateInputIntegerNumber(input);
+            int attemptLimiter = int.Parse(input);
             Console.WriteLine("Press \"Enter\" to start the game.");
             Console.ReadKey();
             Console.Clear();
 
             //Player 1 part
-            Console.WriteLine("Player 1, please, enter the integer number: ");
+            Console.WriteLine($"{firstPlayerName}, please, enter the integer number: ");
 
             input = Console.ReadLine();
 
-            int theNumber = ValidateInput(input);
+            input = ValidateInputIntegerNumber(input);
+            int theNumber = int.Parse(input);
 
             Console.WriteLine("Great, the number is valid! Press \"Enter\" to continue.");
             Console.ReadKey();
             Console.Clear();
 
             //Player 2 part
-            Console.WriteLine($"Player 2, you should guess the number of Player 1. You have {attemptLimiter} attempts.");
+            Console.WriteLine($"{secondPlayerName}, you should guess the number of {firstPlayerName}. You have {attemptLimiter} attempts.");
 
             int guess;
             int attemptCounter = 0;
+
             do
             {
                 if (attemptCounter == attemptLimiter)
                 {
-                    Console.WriteLine($"You loose! You spent all {attemptLimiter} attempts.");
+                    Console.WriteLine($"You loose, {secondPlayerName}! You spent all {attemptLimiter} attempts.");
                     break;
                 }
                 attemptCounter++;
                 Console.WriteLine($"Attempt #{attemptCounter}");
                 Console.WriteLine("Enter the integer number: ");
                 input = Console.ReadLine();
-                guess = ValidateInput(input);
+                input = ValidateInputIntegerNumber(input);
+                guess = int.Parse(input);
 
                 if (guess > theNumber)
                 {
@@ -57,14 +80,19 @@ namespace GuessNumberGame
                 }
                 else
                 {
-                    Console.WriteLine("Congratulations! You win!");
+                    Console.WriteLine($"Congratulations, {secondPlayerName}! You win!");
                     Console.WriteLine($"You spent {attemptCounter} out of {attemptLimiter} attempts.");
                 }
             } while (guess != theNumber);
             
             Console.ReadKey();
         }
-        public static int ValidateInput(string str)
+        /// <summary>
+        /// Checks whether input string is convertable to integer. If no, requires to input again and checks again.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>String convertable to integer.</returns>
+        public static string ValidateInputIntegerNumber(string str)
         {
             int theNumber;
             while (!int.TryParse(str, out theNumber))
@@ -72,7 +100,27 @@ namespace GuessNumberGame
                 Console.WriteLine("This is not an integer number. Enter the integer number:");
                 str = Console.ReadLine();
             }
-            return int.Parse(str);
+            return str;
+        }
+        /// <summary>
+        /// Check whether input string is null, empty string or whitespace. If yes, requires to input again and repeats the check.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>String which is not null, empty string or whitespace.</returns>
+        public static string ValidateInputString(string str)
+        {
+            bool isValid = false;
+            while (!isValid)
+            {
+                if (!string.IsNullOrEmpty(str) && !string.IsNullOrWhiteSpace(str))
+                {
+                    isValid = true;
+                    break;
+                }
+                Console.WriteLine("Your name cannot be an empty string or whitespace. Please, enter it again: ");
+                str = Console.ReadLine();
+            }
+            return str;
         }
     }
 }
